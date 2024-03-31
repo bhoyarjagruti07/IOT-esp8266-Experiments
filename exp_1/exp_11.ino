c12a06517cfc2905ef0624d68c45d08195104f48
bhoyarjagruti07 <138747421+bhoyarjagruti07@users.noreply.github.com>
2024-03-31 14:41:34 +0530

    Add files via upload

---
+++ exp_1/exp_11.ino
@@ -0,0 +1,44 @@
+/*
+ * Sketch : To test all IOs on ESP8266 Witty Cloud Development Board
+ * Notes : For adding the ESP8266 board
+ */
+
+#define led 2
+#define red 15
+#define green 12
+#define blue 13
+#define ldr A0
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(green, OUTPUT);
+ 
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+  digitalWrite(red, HIGH);
+  delay(1000);
+  digitalWrite(red, LOW);
+  delay(1000);
+  digitalWrite(green, HIGH);
+  delay(1000);
+  digitalWrite(green, LOW);
+  delay(1000);
+  //digitalWrite(blue, HIGH);
+  //delay(1000);
+  //digitalWrite(blue, LOW);
+  //delay(1000);
+  Serial.println(analogRead(ldr));
+ 
+
+}
